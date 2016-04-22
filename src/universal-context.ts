9eb095b17e67abf3374d852788291f2112043bf9
gdi2290 <github@gdi2290.com>
2016-04-22 13:35:42 -0700

    chore: src/universal-context

---
+++ src/universal-context.ts
@@ -0,0 +1,51 @@
+// import 'core-js';
+require('zone.js/dist/zone-node.js');
+// require('zone.js/dist/long-stack-trace-zone');
+
+import {Parse5DomAdapter} from 'angular2/src/platform/server/parse5_adapter';
+Parse5DomAdapter.makeCurrent();
+
+import {bootstrap} from 'angular2-universal/dist/node/platform/node';
+import {serializeDocument, parseDocument} from 'angular2-universal/dist/node/platform/document';
+
+import {Component, ComponentRef, provide, enableProdMode} from 'angular2/core';
+import {DOCUMENT} from 'angular2/platform/common_dom';
+import {DomSharedStylesHost} from 'angular2/src/platform/dom/shared_styles_host';
+
+enableProdMode();
+
+@Component({
+  selector: 'app',
+  styles: [`
+    div {
+      background-color: black;
+    }
+  `],
+  template: `
+  <div>
+    oh hai
+  </div>
+  `
+})
+class App {
+
+}
+
+
+export  function main(document) {
+  return bootstrap(App, [
+
+      provide(DOCUMENT, {
+        useFactory: (domSharedStylesHost: DomSharedStylesHost) => {
+          var doc: any = parseDocument(document);
+          domSharedStylesHost.addHost(doc.head);
+          return doc;
+        },
+        deps: [DomSharedStylesHost]
+      })
+
+    ])
+    .then((cmpRef: ComponentRef) => {
+      return serializeDocument(cmpRef.injector.get(DOCUMENT));
+    });
+};
